90a37ff94b74dba17e92a4462c9d889f52159cf7
Berke Küçük <84639060+BerkeKck@users.noreply.github.com>
2022-09-29 10:25:18 +0300

    Update table.sql

--- DiabetesNurseForm/DiabetesNurseForm/table.sql
+++ DiabetesNurseForm/DiabetesNurseForm/table.sql
@@ -10,3 +10,5 @@ INSERT INTO MC_PARAMETERS_DETAIL(ID, MCP_ID, NAME, NOTE, VALUE, IS_ACTIVE)
 VALUES((SELECT MAX(ID)+1 FROM MC_PARAMETERS_DETAIL), 680, 'Yok', NULL, 2, '1');
 
 COMMIT;
+
+// bu sql komutu veritabanındaki parametrelere yenisini manual eklemek için, var yok komutu ve DM tipini eklerken bu kodu kullandım. 
